ce832b34ea4e0f723ccae7f53f9583c7ee6e24e2
agohorel <alex.oddlogic@gmail.com>
2018-02-18 14:57:27 -0600

    stacked/blurred text effect

--- randomChar.js
+++ randomChar.js
@@ -1,6 +1,10 @@
-var words = ['foxtrot', 'fwolharben', 'fixate'];
+var words = ['foxtrot007', 
+			 '!ellipse666', 
+			 '7dot_fixate.', 
+			 'r9hollow?',
+			 'animeWasAMistake420'];
 var randomChars = ['!', '@', '#', '$', '%', '&', '*', '+', '=', '/', '<', '>', '~', '*', '-', '_', '^', '.', '?', '(', ')', '[', ']', '{', '}'];
-var p = document.getElementById('display');
+var p = document.querySelectorAll('.display');
 var selectedRandomChar, index = 0, i = 0; 
 
 String.prototype.replaceAt = function(index, char) {
@@ -11,13 +15,18 @@ String.prototype.replaceAt = function(index, char) {
 
 setInterval(function(){
 	selectedRandomChar = randomChars[Math.floor(Math.random() * randomChars.length)];
-	p.innerText = words[i].replaceAt(index, selectedRandomChar);
+	
+	for (var j = 0; j < p.length; j++){
+		p[j].innerText = "> " + words[i].replaceAt(index, selectedRandomChar);
+	} 
+			
 	index++;
 	if (index >= words[i].length) {
 		index = 0;
 		i++;
 	}
+
 	if (i >= words.length) {
 		i = 0;
-	}
+	}	
 }, 100);
